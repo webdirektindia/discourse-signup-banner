e61613ea92b61eb91e08b6ac8f5aba57b9439600
Webdirekt India <webdirektindia@gmail.com>
2021-11-23 11:38:05 +0530

    commit-1

--- javascripts/discourse/components/dismissable-banner.js
+++ javascripts/discourse/components/dismissable-banner.js
@@ -4,14 +4,44 @@ import discourseComputed from "discourse-common/utils/decorators";
 import { action } from "@ember/object";
 import { default as getURL } from "discourse-common/lib/get-url";
 import cookie from "discourse/lib/cookie";
+import { withPluginApi } from "discourse/lib/plugin-api";
+
 
 export default Component.extend({
   tagName: "",
-  hidden: true,
-
+  hidden: false,
+  bannerTitle: "",
   init() {
     this._super(...arguments);
     this.appEvents.on("cta:shown", this, this._triggerBanner);
+
+    withPluginApi("0.8.8", (api) => {
+      const currentUser = api.getCurrentUser();
+      //let bannerTitleText = { innerHTML: settings.main_heading_content };
+      let bannerTitleText = "Welcome, {First_name} are you up for a challenge ->";
+      if (currentUser && bannerTitleText) {
+
+        api.container.lookup('store:main').find('user', currentUser.username).then((user) => {
+          let currentUserObj = user;
+          let userFieldsObj = this.site.get("user_fields");
+          if (currentUserObj && currentUserObj.username) {
+            var userFieldValues = currentUserObj.user_fields;
+            let firstnameFieldObj = userFieldsObj.find(obj => obj.name.toLowerCase() == ("First name").toLowerCase());
+            var currentuserFirstName = "";
+            if (firstnameFieldObj) { currentuserFirstName = userFieldValues[firstnameFieldObj.id] }
+
+            if (currentuserFirstName) {
+              bannerTitleText = bannerTitleText.replace('{First_name}', currentuserFirstName);
+              //result = h('div.section-header', bannerTitleText);
+              this.set("bannerTitle", bannerTitleText);
+            }
+          }
+        });
+
+
+      }
+
+    });
   },
 
   willDestroyElement() {
@@ -23,20 +53,20 @@ export default Component.extend({
   },
 
   @discourseComputed("hidden")
-  shouldShow(hidden) {
-    return !hidden;
-  },
+shouldShow(hidden) {
+  return !hidden;
+},
 
-  @action
-  dismissBanner() {
-    this.keyValueStore.setItem("anon-cta-never", "t");
-    this.session.set("showSignupCta", false);
-    this.set("hidden", true);
-  },
+@action
+dismissBanner() {
+  this.keyValueStore.setItem("anon-cta-never", "t");
+  this.session.set("showSignupCta", false);
+  this.set("hidden", true);
+},
 
-  @action
-  showBannerLater() {
-    this.keyValueStore.setItem("anon-cta-hidden", Date.now());
-    this.set("hidden", true)
-  },
+@action
+showBannerLater() {
+  this.keyValueStore.setItem("anon-cta-hidden", Date.now());
+  this.set("hidden", true)
+},
 });
